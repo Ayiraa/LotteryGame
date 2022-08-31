2d5f12a91f7f370579bcf09a5355d108322ba9fe
Karolina <kkitta@spoko.pl>
2022-08-31 12:13:44 +0200

    Removed generating duplicate values from NumberGenerator class

--- src/LotteryGame.java
+++ src/LotteryGame.java
@@ -1,7 +1,7 @@
-public class LoteryGame {
+public class LotteryGame {
     public static void main(String[] args){
-        NumberGenerator generator = new NumberGenerator();
-        System.out.println(generator.randomNumbers());
+        
+        System.out.println(NumberGenerator.randomNumbers());
 
     }
 }
--- src/NumberGenerator.java
+++ src/NumberGenerator.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class NumberGenerator {
+import java.util.*;
+
+public class NumberGenerator {
+
+    public static Set<Integer> randomNumbers(){
+        Random random = new Random();
+        //used hashSet to avoid duplicate values
+        Set<Integer> numbers = new LinkedHashSet<>();
+        while (numbers.size() < 6) {
+            numbers.add(random.nextInt(98)+1);
+        }
+        return numbers;
+    }
+
 }

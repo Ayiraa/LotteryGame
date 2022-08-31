77d9572279e9fa6514e0124e37b48ce25cc9181a
Karolina <kkitta@spoko.pl>
2022-08-31 12:00:55 +0200

    Created necessary classes

---
+++ src/LotteryGame.java
@@ -0,0 +1,7 @@
+public class LoteryGame {
+    public static void main(String[] args){
+        NumberGenerator generator = new NumberGenerator();
+        System.out.println(generator.randomNumbers());
+
+    }
+}

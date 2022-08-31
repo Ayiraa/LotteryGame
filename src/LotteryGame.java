4b2aabaf89115e85e54427de4c8dba5a147556ca
Karolina <kkitta@spoko.pl>
2022-08-31 12:34:14 +0200

    Created UserInputManager class, managing user input

--- src/LotteryGame.java
+++ src/LotteryGame.java
@@ -2,6 +2,7 @@ public class LotteryGame {
     public static void main(String[] args){
         
         System.out.println(NumberGenerator.randomNumbers());
+        System.out.println(UserInputManager.getInput());
 
     }
 }
